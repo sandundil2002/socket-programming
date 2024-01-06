5fcb77b8b81ab1bdef8ea5e64d3ad9f5f06097e7
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-01-06 13:21:02 +0530

    simple chat done...

--- src/main/java/lk/ijse/Client.java
+++ src/main/java/lk/ijse/Client.java
@@ -1,18 +1,27 @@
 package lk.ijse;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         try {
             Socket remoteSocket = new Socket("localhost", 3002);
             DataOutputStream dataOutputStream = new DataOutputStream(remoteSocket.getOutputStream());
-            System.out.println("Hello server...!");
             dataOutputStream.writeUTF("Hello server...!");
-            dataOutputStream.flush();
-            dataOutputStream.close();
-            remoteSocket.close();
+
+            DataInputStream dataInputStream = new DataInputStream(remoteSocket.getInputStream());
+            String s = dataInputStream.readUTF();
+            System.out.println(s);
+
+            while (true){
+                String message = input.nextLine();
+                dataOutputStream.writeUTF(message);
+                System.out.println(dataInputStream.readUTF());
+            }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
--- src/main/java/lk/ijse/Server.java
+++ src/main/java/lk/ijse/Server.java
@@ -1,11 +1,14 @@
 package lk.ijse;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         try {
             ServerSocket serverSocket = new ServerSocket(3002);
             System.out.println("Server is started");
@@ -14,6 +17,16 @@ public class Server {
             DataInputStream dataInputStream = new DataInputStream(localSocket.getInputStream());
             String message = dataInputStream.readUTF();
             System.out.println("Client = " + message);
+
+            DataOutputStream dataOutputStream = new DataOutputStream(localSocket.getOutputStream());
+            dataOutputStream.writeUTF("I'm server how can i help you");
+
+            while (true){
+                System.out.println(dataInputStream.readUTF());
+                String msg = scanner.nextLine();
+                dataOutputStream.writeUTF(msg);
+            }
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
