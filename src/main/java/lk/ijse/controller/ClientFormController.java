1b568f3f2ab7dd963c815d27263828976205a28f
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-01-13 10:09:19 +0530

    front-end done...

---
+++ src/main/java/lk/ijse/controller/ClientFormController.java
@@ -0,0 +1,13 @@
+package lk.ijse.controller;
+
+import com.jfoenix.controls.JFXTextArea;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class ClientFormController {
+    public TextField txtMsg;
+    public JFXTextArea txtField;
+
+    public void btnSendOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/lk/ijse/controller/ServerFormController.java
@@ -0,0 +1,14 @@
+package lk.ijse.controller;
+
+import com.jfoenix.controls.JFXTextArea;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import lk.ijse.Server;
+
+public class ServerFormController {
+    public JFXTextArea txtField;
+    public TextField txtMsg;
+
+    public void btnSendOnAction(ActionEvent actionEvent) {
+    }
+}
